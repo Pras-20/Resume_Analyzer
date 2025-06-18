bcda5ffed2dad61483e878799c9f4dc38d9fa093
Pras-20 <prasannakumarpk2023@gmail.com>
2025-06-18 23:25:21 +0300

    started using hugging face

--- resume-analyzer-backend/analyzer_routes.py
+++ resume-analyzer-backend/analyzer_routes.py
@@ -2,22 +2,24 @@ from flask import Blueprint, request,jsonify
 import fitz 
 from sentence_transformers import SentenceTransformer ,util
 import re 
-from openai import OpenAI
 import pytesseract
 from pdf2image import convert_from_bytes
 from PIL import Image
 import io
+import requests
 
 from auth_utils import token_required
 import os 
 from dotenv import load_dotenv
 
 load_dotenv()
-client=OpenAI(api_key=os.getenv("OPENAI_KEY"))
+
+
 
 analyzer_bp=Blueprint('analyzer',__name__)
 
 model=SentenceTransformer('all-MiniLM-L6-V2')
+api_key=os.getenv('HF_API_KEY')
 
 
 def extract_data_from_pdf(file):
@@ -61,12 +63,12 @@ def generate_improvement_points(jd_text,resume_text):
 
     improvement_points=[]
 
-    for i,jd_sentences in enumerate(jd_sentences):
+    for i,jd_sentence in enumerate(jd_sentences):
         sims=util.cos_sim(jd_embeddings[i],resume_embeddings)
         max_sim=sims.max().item()
 
         if max_sim < 0.6:
-            improvement_points.append("Your resume lacks content related to \"{jd_sentence}\"")
+            improvement_points.append(f'Your resume lacks content related to "{jd_sentence}"')
 
     return improvement_points
 
@@ -74,23 +76,40 @@ def generate_detailed_feedback(jd_text,resume_text):
 
     prompt=f"""
     You are a helpful assistant that reads a job description and a candidate's resume. 
-Based on the content, provide detailed and constructive feedback on how the candidate can improve their resume to better fit the job description.
+    Based on the content, provide detailed and constructive feedback on how the candidate can improve their resume to better fit the job description.
     
-Job Description:
-{jd_text}
+    Job Description:
+    {jd_text}
 
-Resume:
-{resume_text}
+    Resume:
+    {resume_text}
 
-Feedback:
-"""
-    response=client.chat.completions.create(
-        model="gpt-4o-mini",
-        messages=[{"role":"user","content":prompt}],
-        temperature=0.7,
-        max_tokens=500,
-    )
-    return response.choice[0].message.content.strip()
+    Feedback:
+    """
+    
+    headers={
+        "Authorization" : f"Bearer {api_key}"
+    }
+
+    json_data={
+        "inputs":prompt,
+        "parameters":{
+            "max_new_tokens":300
+        }
+    }
+
+    response=requests.post("https://api-inference.huggingface.co/models/mistralai/Mistral-7B-Instruct-v0.3",
+                           headers=headers,
+                           json=json_data
+            )
+    if response.status_code == 200:
+        try:
+            return response.json()[0]["generated_text"].split("Feedback:")[-1].strip()
+        except Exception:
+            return "Unexpected response format from Hugging Face."
+    else:
+        return f"Hugging Face API error: {response.status_code}"
+    
 
 
 
